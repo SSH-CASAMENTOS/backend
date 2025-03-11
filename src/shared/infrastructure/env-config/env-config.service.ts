be0faf5b02f6c153863a83e1562b8322b2c684b6
Bkuste2 <igorribeiro632@gmail.com>
2025-03-10 23:58:48 -0300

    fix: ensure PORT is returned as a number in EnvConfigService

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -4,10 +4,10 @@ import { EnvConfig } from './env-config.interface';
 
 @Injectable()
 export class EnvConfigService implements EnvConfig {
-	constructor(private configService: ConfigService) {}
+	constructor(private configService: ConfigService) { }
 
 	getAppPort(): number {
-		return this.configService.get<number>('PORT')
+		return Number(this.configService.get<number>('PORT'))
 	}
 	getNodeEnv(): string {
 		return this.configService.get<string>('NODE_ENV')
